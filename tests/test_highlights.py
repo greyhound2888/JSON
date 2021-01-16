eba635c7eba6bf29262fb8ff471b75c1be5f67f4
Ryuzaki <ufrshubham@gmail.com>
2021-01-16 23:26:49 +0530

    Added test

---
+++ tests/test_highlights.py
@@ -0,0 +1,13 @@
+import json
+import requests
+
+def test_highlights():
+    try:
+        with open('news.json', mode='r') as f:
+            data = json.load(f)
+            for lit_data in data['highlights']:
+                url = lit_data['image']
+                r = requests.get(url)
+                assert r.status_code == 200, 'Broken url ' + url
+    except IOError as e:
+        assert False, 'Failed to open news.json'
